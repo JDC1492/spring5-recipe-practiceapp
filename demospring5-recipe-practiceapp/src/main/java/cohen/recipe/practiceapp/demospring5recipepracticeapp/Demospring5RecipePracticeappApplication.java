773dc661a42679a469b5c83f365501bf0f98b15c
JDC1492 <Jonathan_Cohen10@yahoo.com>
2022-07-07 15:14:39 -0400

    project initialization

---
+++ demospring5-recipe-practiceapp/src/main/java/cohen/recipe/practiceapp/demospring5recipepracticeapp/Demospring5RecipePracticeappApplication.java
@@ -0,0 +1,13 @@
+package cohen.recipe.practiceapp.demospring5recipepracticeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demospring5RecipePracticeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demospring5RecipePracticeappApplication.class, args);
+	}
+
+}
---
+++ demospring5-recipe-practiceapp/src/test/java/cohen/recipe/practiceapp/demospring5recipepracticeapp/Demospring5RecipePracticeappApplicationTests.java
@@ -0,0 +1,13 @@
+package cohen.recipe.practiceapp.demospring5recipepracticeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demospring5RecipePracticeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
