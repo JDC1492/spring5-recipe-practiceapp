b7edde0f979e18073b2207f7b8d2e7bf66df0ada
Jonathan Cohen <cohen_jonathan@bah.com>
2022-07-15 05:26:34 -0400

    corrected a directory mistake

--- demospring5-recipe-practiceapp/src/test/java/cohen/recipe/practiceapp/demospring5recipepracticeapp/repositories/UnitOfMeasureRepositoryIT.java
+++ demospring5-recipe-practiceapp/src/test/java/cohen/recipe/practiceapp/demospring5recipepracticeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -1,11 +1,15 @@
 package cohen.recipe.practiceapp.demospring5recipepracticeapp.repositories;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import cohen.recipe.practiceapp.demospring5recipepracticeapp.domain.UnitOfMeasure;
 
 
 
@@ -26,5 +30,6 @@ public class UnitOfMeasureRepositoryIT {
     void testFindByDescription() throws Exception{
         Optional<UnitOfMeasure> uomOPtional = unitOfMeasureRepository.findByDescription("Teaspoon");
 
+        assertEquals("Teaspoon", uomOPtional.get().getDescription());
     }
 }
