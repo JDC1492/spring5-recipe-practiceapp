5d6d2953fe20aeee1c896bfe387cf1213d85305d
Jonathan Cohen <cohen_jonathan@bah.com>
2022-07-15 04:48:02 -0400

    created unit of measure repository integration test file

---
+++ demospring5-recipe-practiceapp/src/test/java/cohen/recipe/practiceapp/demospring5recipepracticeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,18 @@
+package cohen.recipe.practiceapp.demospring5recipepracticeapp.repositories;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UnitOfMeasureRepositoryIT {
+    
+    
+    @BeforeEach
+    public void setUp() throws Exception{
+
+    }
+    
+    @Test
+    void testFindByDescription() throws Exception{
+
+    }
+}
